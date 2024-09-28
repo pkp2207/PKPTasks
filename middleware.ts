0e057d63ef80e79b3ec9e99bfe0161d351c1ceb3
Param Kamlesh Pathak <parampathak47@gmail.com>
2024-09-28 12:54:48 +0530

    Update 1

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,8 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware({});
+export default authMiddleware({
+  publicRoutes: ["/"], // Allow access to the root route for everyone
+});
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
